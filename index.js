393b16ee85a65dd3e9f31ba803b67e8a37e818d1
jay cooper <15jacooper@gmail.com>
2023-08-30 20:21:46 -0400

    push

---
+++ index.js
@@ -0,0 +1,58 @@
+/*What is React?
+-JavaScript library created by Facebook
+-Also used by Netflix & Instagram
+-Used to create JS-driven dynamic web apps
+-In that regard, can be compared to Angular & Vue*/
+
+/*What is Redux?
+-a layer on top of React
+-helps with state management of the app
+     -data in the app
+     -UI state of the app*/
+
+
+
+
+
+/*How React Works
+-need components (navbar, search box, footer, etc)
+      1. Components look like HTML templates, but are actually JavaScript XML
+      2. Can contain 'state' (data or UI state)
+      3. Can contain JavaScript for functionality*/
+
+/*Component State
+-JavaScript Object
+-Describes the current state of the component (datta, UI state)
+-The state of a component can be updated over time
+    -a modal could close
+    -the data we output could change*/
+
+
+//*Shopping Cart Component
+{
+    items[
+        {name: 'Pitbull', price: 250.00},
+        {name: 'Siberian Husky', price: 350.00}
+    ]
+}
+
+//Popup Component
+{
+    showPopup: true
+}
+
+{
+    showPopup: false
+}
+
+
+/*Create React App
+-a command line tool to create React apps
+    -dedvelopment server
+    -use ES6 features which aren't normally supported
+    -keep out code modular
+    -use build tools to create optomized mode*/
+
+
+
+
